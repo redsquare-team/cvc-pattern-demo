576e5b73bfd9d42cc043ba8884638b8544196b3d
Yehonal <yehonal.azeroth@gmail.com>
2018-11-18 11:58:46 +0100

    build files

---
+++ build/precache-manifest.9aa126b333d814ea0e1c52d1320a484c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72486cda3e3bc4d0fa5b",
+    "url": "/static/js/main.72486cda.chunk.js"
+  },
+  {
+    "revision": "5254826e8e97f7690ccb",
+    "url": "/static/js/1.5254826e.chunk.js"
+  },
+  {
+    "revision": "72486cda3e3bc4d0fa5b",
+    "url": "/static/css/main.ce382b41.chunk.css"
+  },
+  {
+    "revision": "5254826e8e97f7690ccb",
+    "url": "/static/css/1.5c588d4b.chunk.css"
+  },
+  {
+    "revision": "6f2b1d9736f6cd87127978cae0da69e4",
+    "url": "/index.html"
+  }
+];
